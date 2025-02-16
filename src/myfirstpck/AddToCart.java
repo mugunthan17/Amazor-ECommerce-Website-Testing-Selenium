9a091e4e09d5bc074de7f845a51fabe388107215
Mugunthan N <100777814+mugunthan17@users.noreply.github.com>
2025-02-16 22:04:48 +0530

    Add files via upload

---
+++ src/myfirstpck/AddToCart.java
@@ -0,0 +1,46 @@
+package myfirstpck;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class AddToCart {
+	
+	WebDriver driver;
+	
+	@BeforeTest
+	 public void openWebsite() {
+		    System.setProperty("webdriver.chrome.driver", "E:\\chromedriver115.exe");
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+	        driver.get("https://www.amazon.in/");
+	        System.out.println("\n");
+	        System.out.println(driver.getTitle());
+		}
+	@Test(priority=0)
+	public void validlogin() {
+    	
+	       driver.findElement(By.id("nav-link-accountList")).click();
+	       driver.findElement(By.id("ap_email")).sendKeys("8220636712");
+	       driver.findElement(By.id("continue")).click();
+	       driver.findElement(By.id("ap_password")).sendKeys("1a1a1a1a");
+	       driver.findElement(By.id("signInSubmit")).click();
+	     }
+	@Test(priority=1)
+	public void searchproduct() {
+        
+    	driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Iphone 13");
+        driver.findElement(By.id("nav-search-submit-button")).click();
+        driver.get("https://www.amazon.in/Apple-iPhone-13-Mini-256GB/dp/B09G9BJK4X/ref=sr_1_8?keywords=Iphone+13&qid=1680426517&sr=8-8");
+
+    }
+	@Test(priority=2)
+	public void addtocart() {    
+        
+		driver.findElement(By.id("add-to-cart-button")).click();
+    	driver.findElement(By.className("a-button-text")).click();
+        
+    }   
+}
